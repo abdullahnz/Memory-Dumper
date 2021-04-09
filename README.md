d5ca68a182a68e009956bfdeb3f2433c784baf3a
abdullahnz <edfallennzm@gmail.com>
2021-04-09 20:56:47 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Memdump
+
+To dump memory region of some process with pid.
+
+## Usage
+
+Use `-h` or `--help` argument.
+
+```shell
+usage: memdump.py [-h] -p PID [-o OUT] [-f]
+
+Used for dump memory process.
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -p PID, --pid PID  select process id of the target
+  -o OUT, --out OUT  select output file for dumped raw data (default: none)
+  -f, --force        if maps/mem file is readable
+```
+
+Run with `sudo` or force argument if the user has read permissions to the `mem` and `maps` file.
+
+```shell
+$ sudo python3 memdump.py -p 6766
+[00] 00005555a2ee3000 - 00005555a2ee4000 (1000)
+[01] 00005555a30e3000 - 00005555a30e4000 (1000)
+[02] 00005555a30e4000 - 00005555a30e5000 (1000)
+[03] 00007f25ca3d9000 - 00007f25ca5c0000 (1e7000)
+[04] 00007f25ca5c0000 - 00007f25ca7c0000 (200000)
+[05] 00007f25ca7c0000 - 00007f25ca7c4000 (4000)
+[06] 00007f25ca7c4000 - 00007f25ca7c6000 (2000)
+[07] 00007f25ca7c6000 - 00007f25ca7ca000 (4000)
+[08] 00007f25ca7ca000 - 00007f25ca7f3000 (29000)
+[09] 00007f25ca9c2000 - 00007f25ca9c4000 (2000)
+[10] 00007f25ca9f3000 - 00007f25ca9f4000 (1000)
+[11] 00007f25ca9f4000 - 00007f25ca9f5000 (1000)
+[12] 00007f25ca9f5000 - 00007f25ca9f6000 (1000)
+[13] 00007ffdc5f70000 - 00007ffdc5f91000 (21000)
+[14] 00007ffdc5ff6000 - 00007ffdc5ff9000 (3000)
+[15] 00007ffdc5ff9000 - 00007ffdc5ffb000 (2000)
+[16] ffffffffff600000 - ffffffffff601000 (1000)
+[**] Select Region: 2
+[**] Dump region: 
+00005555a30e4000: 0000000000000000 00005555a30e4008 00007f25ca7c5760
+00005555a30e4018: 0000000000000000 0000000000000000 0000000000000000
+
+[**] Saving into outfile 'out/raw_data.dump' : Done
+```
+
+Raw data in output file,
+
+```sh
+$ hd out/raw_data.dump 
+00000000  00 00 00 00 00 00 00 00  08 40 0e a3 55 55 00 00  |.........@..UU..|
+00000010  60 57 7c ca 25 7f 00 00  00 00 00 00 00 00 00 00  |`W|.%...........|
+00000020  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+*
+00001000
+
+```
+
+## Requirements
+- Run with `sudo` for accessing unreadable file.
+- python version 3.x
---
+++ memdump.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+
+from struct import pack, unpack
+import argparse, os
+
+class Memdump:
+    def __init__(self, pid):
+        self.maps_path = f'/proc/{pid}/maps'
+        self.mem_path  = f'/proc/{pid}/mem'
+    
+    def readMaps(self):
+        maps = open(self.maps_path).read()
+        maps = maps.splitlines()
+
+        result = {}
+
+        for index in range(len(maps)):
+            region     = maps[index].split()[0]
+            start, end = map(lambda x: int(x, 16), region.split('-'))
+            result[index] = {
+                'start'  : start,
+                'length' : end - start
+            }
+        return result
+
+    def dumpRegion(self, address, length):
+        with open(self.mem_path, 'rb') as f:
+            f.seek(address)
+            data = f.read(length)
+            f.close()
+        return data
+
+    def printParsedData(self, raw_data, address):
+        print('[**] Dump region: ')
+        current = 0
+        for offset in range(0, len(raw_data), 8*3):
+            data = [unpack("Q", raw_data[i:i+8].ljust(8, b'\0'))[0] for i in range(offset, offset + (8*3), 8)]
+            if data != current:
+                print(f'{address+offset:016x}: {data[0]:016x} {data[1]:016x} {data[2]:016x}')
+            current = data
+    
+    def error(self, message):
+        print(f'memdump.py: error: {message}')
+        exit(1)
+
+    def dump(self, outfile='raw_data.dump'):
+        mapping_address = self.readMaps()
+
+        for i in range(len(mapping_address)):
+            maps = mapping_address[i]
+            end  = maps['start'] + maps['length']
+            print(f'[{i:02d}] {maps["start"]:016x} - {end:016x} ({maps["length"]:x})')
+
+        index    = int(input('[**] Select Region: '))
+        maps     = mapping_address[index]
+        raw_data = self.dumpRegion(maps['start'], maps['length'])
+        
+        outfile  = f'out/{outfile}'
+        
+        self.printParsedData(raw_data, maps['start'])
+        
+        if 'out' not in os.listdir('.'):
+            os.mkdir('out')
+            
+        print(f'\n[**] Saving into outfile \'{outfile}\' : ', end='')
+
+        with open(outfile, 'wb') as f:
+            f.write(raw_data)
+            f.close()
+            
+        print('Done')
+
+if __name__ == "__main__":
+    # Init some argument parser.
+    parser = argparse.ArgumentParser(description='Used for dump memory process.')
+    parser.add_argument('-p', '--pid', action='store', type=int, required=True, help='select process id of the target')
+    parser.add_argument('-o', '--out', action='store', type=str, help='select output file for dumped raw data (default: none)')
+    parser.add_argument('-f', '--force', action='store_true', help='if maps/mem file is readable')
+    args = parser.parse_args()
+
+    # Memdump class.
+    process = Memdump(args.pid)
+    
+    # Check privilege to access unreadable file.
+    if not args.force:
+        if os.getuid() != 0:
+            process.error(f'this script needs accessing an unreadable file, so try with sudo or with \'force\' argument.')
+    try:
+        if args.out:
+            process.dump(outfile=args.out)
+        else:
+            process.dump()
+    
+    except FileNotFoundError:
+        process.error(f'there is no such process for id: {args.pid}')
+
+    except PermissionError as e:
+        process.error(f'try run this script with sudo (permission denied).')
+
+    
